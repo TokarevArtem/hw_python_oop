e67e41a182587a49da55e89962f6b39f229f6610
Артём Токарев <artemabushahmin2000@gmail.com>
2022-07-25 01:14:13 +0500

    davaix4

--- homework.py
+++ homework.py
@@ -62,8 +62,9 @@ class Running(Training):
     def get_spent_calories(self) -> float:
         coeff_calorie_1 = 18
         coeff_calorie_2 = 20
-        return (coeff_calorie_1 * self.get_mean_speed()
-              - coeff_calorie_2) * self.weight / self.M_IN_KM * (self.duration * 60)
+        return ((coeff_calorie_1 * self.get_mean_speed()
+                 - coeff_calorie_2) * self.weight / self.M_IN_KM
+                * (self.duration * 60))
 
 
 class SportsWalking(Training):
@@ -84,9 +85,8 @@ class SportsWalking(Training):
         calorie_2 = 0.029
         return (
                (calorie_1 * self.weight
-             + (self.get_mean_speed()**2 // self.height)
-              * calorie_2 * self.weight) * (self.duration * 60)
-               )
+                + (self.get_mean_speed()**2 // self.height)
+                * calorie_2 * self.weight) * (self.duration * 60))
 
 
 class Swimming(Training):
@@ -111,7 +111,7 @@ class Swimming(Training):
 
     def get_mean_speed(self) -> float:
         return (self.length_pool * self.count_pool
-              / self.M_IN_KM / self.duration)
+                / self.M_IN_KM / self.duration)
 
 
 def read_package(workout_type: str, data: list) -> Training:
